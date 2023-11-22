16a45aeaa23873792263577440182a0f216a7b77
SalazarRodolfo <151742360+SalazarRodolfo@users.noreply.github.com>
2023-11-22 22:22:59 +0800

    Add files via upload

---
+++ RestaurantPOS/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Restro POS System</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+        .wrapper {
+            width: 100%;
+            height: 45px;
+            background: #f0f3ed;
+            border: 2px solid rgba(240, 243, 237, .2);
+            outline: none;
+            border-radius: 40px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, .1);
+            cursor: pointer;
+            font-size: 16px;
+            color: #fff;
+            font-weight: 600;
+        }
+    </style>
+</head>
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                Restaurant POS
+            </div>
+
+            <div class="links">
+                <a href="Restro/admin/" class="wrapper">Admin Log In</a>
+                <a href="Restro/cashier/" class="wrapper">Cashier Log In</a>
+                <a href="Restro/customer" class="wrapper">Customer Log In</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
